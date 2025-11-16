3fa079b0897ae6fbd8bcb17c3551e9ec3f603300
Su-summertimeSadness <aleksandrandrph@gmail.com>
2025-11-16 12:11:04 +0300

    up

--- backend/src/bot/middleware/db.py
+++ backend/src/bot/middleware/db.py
@@ -1,15 +1,18 @@
 # bot/middleware/db.py
 from aiogram import BaseMiddleware
-from typing import Callable, Dict, Any, Awaitable, Message
+from aiogram.types import Message, CallbackQuery  # Импортируйте отсюда
+from typing import Callable, Dict, Any, Awaitable, Union
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from db.database import async_session_maker
 
 class DbMiddleware(BaseMiddleware):
     async def __call__(
         self,
-        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
+        handler: Callable[[Union[Message, CallbackQuery], Dict[str, Any]], Awaitable[Any]],
+        event: Union[Message, CallbackQuery],
         data: Dict[str, Any]
     ) -> Any:
         async with async_session_maker() as session:
-            data['session'] = session
+            data['db'] = session
             return await handler(event, data)
