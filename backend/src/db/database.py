c44d0ae9669ccf69d0d3d891f7f087f7350243b2
Su-summertimeSadness <aleksandrandrph@gmail.com>
2025-11-25 16:18:51 +0300

    bot

--- backend/src/db/database.py
+++ backend/src/db/database.py
@@ -3,6 +3,7 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 import asyncpg
 import logging
+from .base  import Base
 
 logger = logging.getLogger(__name__)
 
