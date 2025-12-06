017424f0a8763d601fcb1e0d3f6585e8267853b3
Su-summertimeSadness <aleksandrandrph@gmail.com>
2025-12-06 11:11:19 +0300

    dialog

--- backend/src/bot/services/__init__.py
+++ backend/src/bot/services/__init__.py
@@ -6,11 +6,13 @@ from .notification_service import (
 
 from .assignment_service import QuestionAssignmentService  # Добавьте эту строку
 from .qa_service import answer_question_internal  # Если используется
+from .dialog_service import DialogService  # Добавьте эту строку
 
 __all__ = [
     'notify_pharmacists_about_new_question',
     'notify_about_clarification',
     'get_online_pharmacists',
     'QuestionAssignmentService',  # Добавьте эту строку
-    'answer_question_internal',   # Если используется
+    'answer_question_internal',
+    "DialogService"   # Если используется
 ]
